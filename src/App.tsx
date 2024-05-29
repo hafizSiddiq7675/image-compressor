c7fadb8e04aa953dd29ab2a5e129e24ff64b6f97
hafizSiddiq7675 <hafiz.siddiq.umer@gmail.com>
2024-05-29 09:47:44 +0500

    re intialized

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { ConfigProvider } from "antd";
+import { observer } from "mobx-react-lite";
+import { gstate } from "./global";
+import { ContextAction } from "./ContextAction";
+import { Analytics } from "@vercel/analytics/react";
+import { Loading } from "./components/Loading";
+
+export const App = observer(() => {
+  return (
+    <ConfigProvider
+      locale={gstate.locale?.antLocale}
+      theme={{
+        token: {
+          colorPrimary: "#1da565",
+          colorLink: "#1da565",
+          colorSuccess: "#1da565",
+        },
+      }}
+    >
+      <ContextAction />
+      {import.meta.env.MODE === "production" && <Analytics />}
+      {gstate.page}
+      {gstate.loading && <Loading />}
+    </ConfigProvider>
+  );
+});
